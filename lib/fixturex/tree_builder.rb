9efb5482a1a466037bcbcf1d9b88f3d32bba3afa
artemave <artemave@gmail.com>
2025-06-17 12:14:59 +0200

    Prefer namespaced constant when figuring out associated model class

--- lib/fixturex/tree_builder.rb
+++ lib/fixturex/tree_builder.rb
@@ -91,11 +91,11 @@ module Fixturex
     # rubocop:enable Metrics/AbcSize
 
     def association_model_class(association)
-      class_name = association.class_name
+      namespace = association.active_record.name.split('::')[0..-2].join('::')
+      class_name = [namespace, association.class_name].filter(&:present?).join('::')
 
       unless Object.const_defined?(class_name)
-        namespace = association.active_record.name.split('::')[0..-2].join('::')
-        class_name = "#{namespace}::#{class_name}"
+        class_name = association.class_name
       end
 
       class_name.constantize
